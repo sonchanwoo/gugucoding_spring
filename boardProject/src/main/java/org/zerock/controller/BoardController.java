9e567f4b3d5ada2f612f66e4cbc082d07e128df3
sonchanwoo <chanchan1116@naver.com>
2022-01-25 23:06:15 +0900

    controller (책내용 완벽히는 미반영)

---
+++ boardProject/src/main/java/org/zerock/controller/BoardController.java
@@ -0,0 +1,51 @@
+package org.zerock.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.zerock.domain.BoardVO;
+import org.zerock.service.BoardService;
+
+import lombok.AllArgsConstructor;
+
+@Controller
+@AllArgsConstructor
+@RequestMapping("/board")
+public class BoardController {
+    private BoardService service;
+    
+    @GetMapping("/list")
+    public void list(Model model) {
+        model.addAttribute("list",service.getList());
+    }
+    
+    @GetMapping("/register")
+    public void register() {        
+    }
+    
+    @PostMapping("/register")
+    public String register(BoardVO vo) {
+        service.register(vo);
+        return "redirect:/board/list";
+    }
+    
+    @GetMapping({"/get","/modify"})
+    public void get(Long bno, Model model) {
+        model.addAttribute("vo",service.get(bno));
+    }
+    
+    @PostMapping("/modify")
+    public String modify(BoardVO vo){
+        service.modify(vo);
+        return "redirect:/board/list";
+    }
+    
+    @PostMapping("/remove")
+    public String remove(Long bno){
+        service.remove(bno);
+        return "redirect:/board/list";
+    }
+}
---
+++ boardProject/src/test/java/org/zerock/controller/BoardControllerTest.java
@@ -0,0 +1,138 @@
+package org.zerock.controller;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.context.WebApplicationContext;
+import org.springframework.web.servlet.ModelAndView;
+
+import lombok.Setter;
+import lombok.extern.log4j.Log4j;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@WebAppConfiguration//WebApplicationContext를 사용하가 위함
+@ContextConfiguration({
+    "file:src/main/webapp/WEB-INF/spring/root-context.xml",
+"file:src/main/webapp/WEB-INF/spring/appServlet/servlet-context.xml"})
+@Log4j
+public class BoardControllerTest {
+    @Setter(onMethod_=@Autowired)
+    private WebApplicationContext ctx;
+
+    private MockMvc mockMvc;
+
+    @Before
+    public void setUp() {
+        this.mockMvc = MockMvcBuilders.webAppContextSetup(ctx).build();
+    }
+
+    @Test
+    public void testList() throws Exception{
+        ModelAndView modelAndView = mockMvc.perform(MockMvcRequestBuilders.get("/board/list"))//원하는 방식과, 경로
+                .andReturn().getModelAndView();
+
+        ModelMap modelMap = modelAndView.getModelMap();
+
+        String viewName = modelAndView.getViewName();
+
+
+        log.info("\n...................\n modelMap : "
+                +modelMap+"\n viewName : "
+                +viewName+"\n...................");
+    }
+
+    @Test
+    public void testRegister() throws Exception{
+        ModelAndView modelAndView = mockMvc.perform(MockMvcRequestBuilders.get("/board/register"))//원하는 방식과, 경로
+                .andReturn().getModelAndView();
+
+        ModelMap modelMap = modelAndView.getModelMap();
+
+        String viewName = modelAndView.getViewName();
+
+
+        log.info("\n...................\n modelMap : "
+                +modelMap+"\n viewName : "
+                +viewName+"\n...................");
+    }
+
+    @Test
+    public void testRegisterPost() throws Exception{
+        ModelAndView modelAndView = mockMvc.perform(MockMvcRequestBuilders.post("/board/register")
+                .param("title", "제에목")
+                .param("content", "내에용")
+                .param("writer", "작성자")
+                )
+                .andReturn().getModelAndView();
+
+        ModelMap modelMap = modelAndView.getModelMap();
+
+        String viewName = modelAndView.getViewName();
+
+
+        log.info("\n...................\n modelMap : "
+                +modelMap+"\n viewName : "
+                +viewName+"\n...................");
+    }
+    
+    @Test
+    public void testGetAndModify() throws Exception{
+        ModelAndView modelAndView = mockMvc.perform(MockMvcRequestBuilders.get("/board/get")
+                .param("bno", "4"))//원하는 방식과, 경로
+                .andReturn().getModelAndView();
+
+        ModelMap modelMap = modelAndView.getModelMap();
+
+        String viewName = modelAndView.getViewName();
+
+
+        log.info("\n...................\n modelMap : "
+                +modelMap+"\n viewName : "
+                +viewName+"\n...................");
+    }
+    
+    @Test
+    public void testModify() throws Exception{
+        ModelAndView modelAndView = mockMvc.perform(MockMvcRequestBuilders.post("/board/modify")
+                .param("bno", "4")
+                .param("title", "제에목")
+                .param("content", "내에용")
+                .param("writer", "작성자")
+                )
+                .andReturn().getModelAndView();
+
+        ModelMap modelMap = modelAndView.getModelMap();
+
+        String viewName = modelAndView.getViewName();
+
+
+        log.info("\n...................\n modelMap : "
+                +modelMap+"\n viewName : "
+                +viewName+"\n...................");
+    }
+    
+    @Test
+    public void testRemove() throws Exception{
+        ModelAndView modelAndView = mockMvc.perform(MockMvcRequestBuilders.post("/board/remove")
+                .param("bno", "4")
+                )
+                .andReturn().getModelAndView();
+
+        ModelMap modelMap = modelAndView.getModelMap();
+
+        String viewName = modelAndView.getViewName();
+
+
+        log.info("\n...................\n modelMap : "
+                +modelMap+"\n viewName : "
+                +viewName+"\n...................");
+    }
+}
